8c1296e1b476a896b36c21727b51bb1acac94c81
Jagnesh Chawla <jagnesh_chawla@hotmail.com>
2026-06-25 18:56:57 +0530

    enum added

--- db/schema.js
+++ db/schema.js
@@ -1,19 +1,20 @@
 import {
   integer,
+  pgEnum,
   pgTable,
   text,
   timestamp,
   uuid,
   varchar,
 } from "drizzle-orm/pg-core";
-
+export const userRoleEnum = pgEnum("user_role", ["USER", "ADMIN"]);
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   password: text().notNull(),
   salt: text().notNull(),
-  role: varchar({ length: 255 }).notNull().default("user"),
+  role: userRoleEnum().notNull().default("USER"),
 });
 
 export const userSessions = pgTable("user_sessions", {
